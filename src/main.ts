4d7ee11d2bd9b3684761fd9e8a87287c683759d5
Johnson Lai <jlwhoo7@gmail.com>
2023-08-22 11:57:45 +0800

    created initial code push

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,37 @@
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-function createWindow() {
+import { app, BrowserWindow, ipcMain } from "electron";
+import * as path from "path";
+import * as fs from "fs";
+import axios from "axios";
+
+async function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 600,
+    width: 428,
+    height: 926,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false, // Important for IPC
     },
-    width: 800,
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  const opt = {
+    userAgent:
+      "Mozilla/5.0 (iPhone; CPU iPhone OS 17_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1 standalone",
+  };
+  // await mainWindow.loadURL("https://www.friend.tech", opt);
+  await mainWindow.loadURL(
+    "https://www.friend.tech/rooms/0x9793d62bdfb6a170cd68df9a19cbf021b8fd7d43",
+    opt
+  );
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  await mainWindow.webContents.openDevTools({
+    mode: "detach",
+  });
 }
 
 // This method will be called when Electron has finished
@@ -42,3 +58,113 @@ app.on("window-all-closed", () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+
+interface CategorizedData {
+  message: string;
+  name: string;
+  time: string;
+}
+
+ipcMain.on("messages", async (event, args, isLatest) => {
+  const messages = args;
+
+  if (isLatest) {
+    fs.writeFileSync("messages.json", JSON.stringify(messages));
+  } else if (fs.existsSync("messages.json")) {
+    const oldMessages = JSON.parse(fs.readFileSync("messages.json", "utf-8"));
+
+    const categorizedData: CategorizedData[] = categorizeData(messages);
+    const latestMessage = categorizedData[categorizedData.length - 1];
+    const oldCategorizedMessage = categorizeData(oldMessages);
+    const oldLatestMessage =
+      oldCategorizedMessage[oldCategorizedMessage.length - 1];
+    if (oldLatestMessage.message === latestMessage.message) return;
+
+    console.log(`[CHATGPT] ${latestMessage.name}: ${latestMessage.message}`);
+    if (latestMessage.name === "superoo7 | Hooga 🍌") {
+      fs.writeFileSync("messages.json", JSON.stringify(messages));
+      return;
+    }
+    const spamDetection = await askGpt(
+      `Act as a spam detector, I will give you a message, you will tell me whether it is spam or not.
+Format the response as JSON like following:
+{
+  "spam": true
+}`,
+      latestMessage.message
+    );
+    const isSpam = JSON.parse(spamDetection).spam;
+    if (isSpam === true) {
+      event.sender.send(
+        "reply",
+        `@${latestMessage.name}: remind you to refrain from spamming within our friendtech group to maintain a positive and respectful environment 🚫⚖️`
+      );
+    } else {
+      const replyMessage = await askGpt(
+        `Based on user's message, please reply in all caps and super excited.
+              
+Reply answer in 1 sentence.`,
+        `${latestMessage.name}: ${latestMessage.message}}`
+      );
+      const isReplySpamMessage = await askGpt(
+        `Act as a spam detector, I will give you a message, you will tell me whether it is spam or not.
+Format the response as JSON like following:
+{
+  "spam": true
+}`,
+        replyMessage
+      );
+      const isReplySpam = JSON.parse(isReplySpamMessage).spam;
+      if (isReplySpam === true) {
+        event.sender.send(
+          "reply",
+          `@${latestMessage.name}: remind you to refrain from spamming within our friendtech group to maintain a positive and respectful environment 🚫⚖️`
+        );
+      } else {
+        event.sender.send("reply", replyMessage);
+      }
+    }
+  } else {
+    fs.writeFileSync("messages.json", JSON.stringify(messages));
+  }
+});
+
+function categorizeData(data: string[]) {
+  const result = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].includes("·")) {
+      const [name, time] = data[i].split(" · ");
+      result.push({
+        message: data[i - 1],
+        name: name.trim(),
+        time: time.trim(),
+      });
+    }
+  }
+  return result;
+}
+
+const askGpt = async (systemMessage: string, userMessage: string) => {
+  const data = await axios({
+    method: "POST",
+    url: "https://api.openai.com/v1/chat/completions",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    data: {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: systemMessage,
+        },
+        {
+          role: "user",
+          content: userMessage,
+        },
+      ],
+    },
+  });
+  return data.data.choices[0].message.content;
+};
--- src/preload.ts
+++ src/preload.ts
@@ -1,14 +1,97 @@
 // All of the Node.js APIs are available in the preload process.
+
+import { ipcRenderer } from "electron";
+
 // It has the same sandbox as a Chrome extension.
 window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector: string, text: string) => {
-    const element = document.getElementById(selector);
-    if (element) {
-      element.innerText = text;
-    }
-  };
+  // To use the function and get the messages:
+});
 
-  for (const type of ["chrome", "node", "electron"]) {
-    replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
+function extractMessages() {
+  // Get the container of all messages
+  const container = document.getElementById("messages-container");
+
+  // Get all div elements inside the container
+  const divElements: any = container.getElementsByTagName("div");
+
+  // An array to store messages
+  const messages = [];
+
+  for (const div of divElements) {
+    // Check if the div element has a direct child that's a text node
+    if (div.childNodes.length && div.childNodes[0].nodeType === 3) {
+      // Push the text content to the messages array
+      messages.push(div.childNodes[0].nodeValue.trim());
+    }
   }
+
+  return messages;
+}
+
+function typeInput(input: string) {
+  const inputForm = document.querySelector<HTMLTextAreaElement>(
+    "textarea.form-control"
+  );
+  if (!inputForm) {
+    console.error("Textarea not found");
+    return;
+  }
+
+  // Set the value
+  inputForm.innerText = input;
+
+  // Trigger an input event
+  const inputEvent = new Event("input", { bubbles: true });
+  inputForm.dispatchEvent(inputEvent);
+
+  // Trigger a change event
+  const changeEvent = new Event("change", { bubbles: true });
+  inputForm.dispatchEvent(changeEvent);
+
+  // Invoke the sendEnter function
+  sendEnter();
+}
+
+function sendEnter() {
+  const submitBtn = document.querySelector<HTMLButtonElement>(
+    "button#send-message-btn"
+  );
+
+  if (submitBtn) {
+    // Directly invoke the click method
+    submitBtn.click();
+
+    // Simulate touchend event, common for mobile taps
+    const touchEvent = new Event("touchend", { bubbles: true });
+    submitBtn.dispatchEvent(touchEvent);
+  } else {
+    console.error("Button not found");
+  }
+}
+
+(window as any).typeInput = typeInput;
+(window as any).sendEnter = sendEnter;
+(window as any).latestMessage = "";
+
+// Interval
+setInterval(() => {
+  const messages = extractMessages();
+  if (messages.length === 0) return;
+  if (messages[messages.length - 2] === (window as any).latestMessage) {
+    ipcRenderer.send("messages", messages, true);
+    (window as any).latestMessage = "";
+  } else ipcRenderer.send("messages", messages);
+  return;
+}, 10000);
+
+setTimeout(() => {
+  // reload
+  location.reload();
+}, 5 * 60 * 1000);
+
+// Listen for the response from the main process
+ipcRenderer.on("reply", (event, data) => {
+  console.log("REPLY DATA:", data);
+  (window as any).latestMessage = data;
+  typeInput(data);
 });
